cf3da86cd169f79744a4a2766e5367a41b046675
Lewis England <lewis2004@hotmail.com>
2020-04-14 22:34:56 +0100

    Enabling PWA

---
+++ precache-manifest.67387720bf0a3ba7313bddf2405f1d06.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2293ab1257ee7c66417",
+    "url": "/css/app.c2f575c6.css"
+  },
+  {
+    "revision": "2e05cb9abca3265e9f55",
+    "url": "/css/chunk-vendors.d557022b.css"
+  },
+  {
+    "revision": "937c66d9a9a99710f6dcf2016a5915f4",
+    "url": "/img/wherehaveibeen-screenshot.png"
+  },
+  {
+    "revision": "f38024c3f0501285d04abd5ba1f2f026",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2293ab1257ee7c66417",
+    "url": "/js/app.d809ad0f.js"
+  },
+  {
+    "revision": "2e05cb9abca3265e9f55",
+    "url": "/js/chunk-vendors.f3a59dee.js"
+  },
+  {
+    "revision": "22982c4fb3da9dd23974229c918a044f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "a4b243bccb600245696bfc67868b3310",
+    "url": "/privacyPolicy.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
