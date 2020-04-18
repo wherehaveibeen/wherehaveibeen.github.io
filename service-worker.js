1e90eea0875f7d43c1653c81fa6affec3e52264e
SplinterHead <lewis2004@hotmail.com>
2020-04-18 22:53:04 +0000

    Deploying to master from  @ ccd5f162920c77e2703066f7e7e0fc8558475bcd 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ce933b1950cf6d0d6e0b9b8ebe64730e.js"
+  "/precache-manifest.92264448a8fc1baa49936c40562d5f8c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "wherehaveibeen"});
